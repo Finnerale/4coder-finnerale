0813a6790a4054751fb332c1c0cd227a3dd156f6
Leopold Luley <git@leopoldluley.de>
2020-12-25 20:02:08 +0100

    Cleanup some whitespace.

--- 4coder_finnerale.cpp
+++ 4coder_finnerale.cpp
@@ -17,17 +17,17 @@ CUSTOM_COMMAND_SIG(goto_compilation_jump)
 CUSTOM_DOC("Jump to places from the compilation buffer")
 {
     Heap *heap = &global_heap;
-    
+
     View_ID view = get_active_view(app, Access_ReadVisible);
     String_Const_u8 name = string_u8_litexpr("*compilation*");
     Buffer_ID buffer = get_buffer_by_name(app, name, Access_ReadVisible);
     Marker_List *list = language_get_or_make_list_for_buffer(app, heap, buffer);
-    
+
     i64 pos = view_get_cursor_pos(app, view);
     Buffer_Cursor cursor = buffer_compute_cursor(app, buffer, seek_pos(pos));
-    
+
     i32 list_index = get_index_exact_from_list(app, list, cursor.line);
-    
+
     if (list_index >= 0){
         ID_Pos_Jump_Location location = {};
         if (get_jump_from_list(app, list, list_index, &location)){
@@ -36,7 +36,7 @@ CUSTOM_DOC("Jump to places from the compilation buffer")
             }
         }
     }
-    
+
     // TODO(Leopold): I don't think this should be necessary?
     goto_next_jump(app);
 }
@@ -79,16 +79,16 @@ function void
 custom_render_buffer(Application_Links *app, View_ID view_id, Face_ID face_id, Buffer_ID buffer, Text_Layout_ID text_layout_id, Rect_f32 rect, Frame_Info frame_info)
 {
     ProfileScope(app, "[Custom] render buffer");
-    
+
     View_ID active_view = get_active_view(app, Access_Always);
     b32 is_active_view = active_view == view_id;
     Rect_f32 prev_clip = draw_set_clip(app, rect);
-    
+
     // NOTE(allen): Cursor shape
     Face_Metrics metrics = get_face_metrics(app, face_id);
     f32 cursor_roundness = (metrics.normal_advance*0.5f)*0.9f;
     f32 mark_thickness = 2.f;
-    
+
     // Language **language = buffer_get_language(app, buffer);
     Token_Array token_array = get_token_array_from_buffer(app, buffer);
     if (token_array.tokens != 0)
@@ -112,16 +112,16 @@ custom_render_buffer(Application_Links *app, View_ID view_id, Face_ID face_id, B
         Range_i64 visible_range = text_layout_get_visible_range(app, text_layout_id);
         paint_text_color_fcolor(app, text_layout_id, visible_range, fcolor_id(defcolor_text_default));
     }
-    
+
     i64 cursor_pos = view_correct_cursor(app, view_id);
     view_correct_mark(app, view_id);
-    
+
     // NOTE(allen): Scope highlight
     if (global_config.use_scope_highlight){
         Color_Array colors = finalize_color_array(defcolor_back_cycle);
         draw_scope_highlight(app, buffer, text_layout_id, cursor_pos, colors.vals, colors.count);
     }
-    
+
     if (global_config.use_error_highlight || global_config.use_jump_highlight){
         // NOTE(allen): Error highlight
         String_Const_u8 name = string_u8_litexpr("*compilation*");
@@ -130,7 +130,7 @@ custom_render_buffer(Application_Links *app, View_ID view_id, Face_ID face_id, B
             draw_jump_highlights(app, buffer, text_layout_id, compilation_buffer,
                                  fcolor_id(defcolor_highlight_junk));
         }
-        
+
         // NOTE(allen): Search highlight
         if (global_config.use_jump_highlight){
             Buffer_ID jump_buffer = get_locked_jump_buffer(app);
@@ -140,20 +140,20 @@ custom_render_buffer(Application_Links *app, View_ID view_id, Face_ID face_id, B
             }
         }
     }
-    
+
     // NOTE(allen): Color parens
     if (global_config.use_paren_helper){
         Color_Array colors = finalize_color_array(defcolor_text_cycle);
         draw_paren_highlight(app, buffer, text_layout_id, cursor_pos, colors.vals, colors.count);
     }
-    
+
     // NOTE(allen): Line highlight
     if (global_config.highlight_line_at_cursor && is_active_view){
         i64 line_number = get_line_number_from_pos(app, buffer, cursor_pos);
         draw_line_highlight(app, text_layout_id, line_number,
                             fcolor_id(defcolor_highlight_cursor_line));
     }
-    
+
     // NOTE(allen): Whitespace highlight
     b64 show_whitespace = false;
     view_get_setting(app, view_id, ViewSetting_ShowWhitespace, &show_whitespace);
@@ -165,10 +165,10 @@ custom_render_buffer(Application_Links *app, View_ID view_id, Face_ID face_id, B
             draw_whitespace_highlight(app, text_layout_id, &token_array, cursor_roundness);
         }
     }
-    
+
     if (true) {
-    // NOTE(Leopold): Cursor in Fleury style
-    F4_RenderCursor(app, view_id, is_active_view, buffer, text_layout_id, cursor_roundness, mark_thickness, frame_info);
+	// NOTE(Leopold): Cursor in Fleury style
+	F4_RenderCursor(app, view_id, is_active_view, buffer, text_layout_id, cursor_roundness, mark_thickness, frame_info);
     } else {
         // NOTE(allen): Cursor
         switch (fcoder_mode){
@@ -182,27 +182,27 @@ custom_render_buffer(Application_Links *app, View_ID view_id, Face_ID face_id, B
             }break;
         }
     }
-    
+
     // NOTE(allen): Fade ranges
     paint_fade_ranges(app, text_layout_id, buffer);
-    
+
     // NOTE(allen): put the actual text on the actual screen
     draw_text_layout_default(app, text_layout_id);
-    
+
     draw_set_clip(app, prev_clip);
 }
 
 BUFFER_HOOK_SIG(custom_begin_buffer)
 {
     language_begin_buffer(app, buffer_id);
-    
+
     return 0;
 }
 
 BUFFER_EDIT_RANGE_SIG(custom_buffer_edit_range)
 {
     language_buffer_edit_range(app, buffer_id, new_range, old_cursor_range);
-    
+
     return 0;
 }
 
@@ -212,16 +212,16 @@ custom_render_caller(Application_Links *app, Frame_Info frame_info, View_ID view
     ProfileScope(app, "[Custom] render caller");
     View_ID active_view = get_active_view(app, Access_Always);
     b32 is_active_view = (active_view == view_id);
-    
+
     Rect_f32 region = draw_background_and_margin(app, view_id, is_active_view);
     Rect_f32 prev_clip = draw_set_clip(app, region);
-    
+
     Buffer_ID buffer = view_get_buffer(app, view_id, Access_Always);
     Face_ID face_id = get_face_id(app, buffer);
     Face_Metrics face_metrics = get_face_metrics(app, face_id);
     f32 line_height = face_metrics.line_height;
     f32 digit_advance = face_metrics.decimal_digit_advance;
-    
+
     // NOTE(allen): file bar
     b64 showing_file_bar = false;
     if (view_get_setting(app, view_id, ViewSetting_ShowFileBar, &showing_file_bar) && showing_file_bar){
@@ -229,9 +229,9 @@ custom_render_caller(Application_Links *app, Frame_Info frame_info, View_ID view
         draw_file_bar(app, view_id, buffer, face_id, pair.min);
         region = pair.max;
     }
-    
+
     Buffer_Scroll scroll = view_get_buffer_scroll(app, view_id);
-    
+
     Buffer_Point_Delta_Result delta = delta_apply(app, view_id,
                                                   frame_info.animation_dt, scroll);
     if (!block_match_struct(&scroll.position, &delta.point)){
@@ -241,10 +241,10 @@ custom_render_caller(Application_Links *app, Frame_Info frame_info, View_ID view
     if (delta.still_animating){
         animate_in_n_milliseconds(app, 0);
     }
-    
+
     // NOTE(allen): query bars
     region = default_draw_query_bars(app, region, view_id, face_id);
-    
+
     // NOTE(allen): FPS hud
     if (show_fps_hud){
         Rect_f32_Pair pair = layout_fps_hud_on_bottom(region, line_height);
@@ -252,7 +252,7 @@ custom_render_caller(Application_Links *app, Frame_Info frame_info, View_ID view
         region = pair.min;
         animate_in_n_milliseconds(app, 1000);
     }
-    
+
     // NOTE(allen): layout line numbers
     Rect_f32 line_number_rect = {};
     if (global_config.show_line_number_margins){
@@ -260,20 +260,20 @@ custom_render_caller(Application_Links *app, Frame_Info frame_info, View_ID view
         line_number_rect = pair.min;
         region = pair.max;
     }
-    
+
     // NOTE(allen): begin buffer render
     Buffer_Point buffer_point = scroll.position;
     Text_Layout_ID text_layout_id = text_layout_create(app, buffer, region, buffer_point);
-    
+
     // NOTE(allen): draw line numbers
     if (global_config.show_line_number_margins){
         draw_line_number_margin(app, view_id, buffer, face_id, text_layout_id, line_number_rect);
     }
-    
+
     // NOTE(allen): draw the buffer
     // NOTE(Leopold): but in my way
     custom_render_buffer(app, view_id, face_id, buffer, text_layout_id, region, frame_info);
-    
+
     text_layout_free(app, text_layout_id);
     draw_set_clip(app, prev_clip);
 }
@@ -292,35 +292,35 @@ void
 custom_layer_init(Application_Links *app)
 {
     Thread_Context* tctx = get_thread_context(app);
-    
+
     default_framework_init(app);
     set_all_default_hooks(app);
-    
+
     init_ext_language();
     // TODO(Leopold): Rust must be the first, otherwise cpp will consume the error messages incorrectly.
     init_language_rust();
     init_language_cpp();
     init_language_odin();
     finalize_languages(app);
-    
+
     mapping_init(tctx, &framework_mapping);
     setup_default_mapping(&framework_mapping, mapid_global, mapid_file, mapid_code);
     {
         MappingScope();
         SelectMapping(&framework_mapping);
-        
+
         SelectMap(mapid_global);
         Bind(load_project, KeyCode_P, KeyCode_Alt);
-        
+
         SelectMap(mapid_file);
         Bind(copy_line, KeyCode_C, KeyCode_Control, KeyCode_Shift);
         Bind(half_page_up, KeyCode_PageUp);
         Bind(half_page_down, KeyCode_PageDown);
-        
+
         SelectMap(mapid_code);
         Bind(goto_compilation_jump, KeyCode_G, KeyCode_Alt);
     }
-    
+
     set_custom_hook(app, HookID_BeginBuffer, custom_begin_buffer);
     set_custom_hook(app, HookID_BufferEditRange, custom_buffer_edit_range);
     set_custom_hook(app, HookID_RenderCaller, custom_render_caller);
